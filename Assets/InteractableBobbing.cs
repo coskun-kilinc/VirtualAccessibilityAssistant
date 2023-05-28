a0fca75c5619ca986e37b0f9feee83dde4f0e777
Coskun Kilinc <82747562+coskun-kilinc@users.noreply.github.com>
2023-05-28 20:34:52 +1000

    initial code commit

---
+++ Assets/InteractableBobbing.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBobbing : MonoBehaviour
+{
+    public float bobSpeed = 1f; // Speed at which the object bobs up and down
+    public float bobHeight = 0.5f; // Height of the bobbing motion
+    public float rotateSpeed = 30f; // Speed at which the object rotates
+    private Vector3 startPosition; // Initial position of the object
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Bobbing motion
+        Vector3 bobPosition = startPosition + Vector3.up * Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+        transform.position = bobPosition;
+
+        // Rotating motion
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+}
